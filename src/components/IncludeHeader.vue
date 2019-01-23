583442ed90555a10a26d0d833a3c9a0158ba92de
Gabriel Freire <gabrielfvale@gmail.com>
2019-01-23 18:32:34 -0300

    IncludeHeader: logo animation

--- src/components/IncludeHeader.vue
+++ src/components/IncludeHeader.vue
@@ -9,19 +9,32 @@
   /* logo img rules start */
   .logo {
     margin-left: 50px;
+    transition: transform .5s ease-in-out;
   }
 
   .logo svg {
     max-height: 100px;
   }
 
-  .logo-group {
-    transition: transform 1s linear;
+  .logo:hover {
+    transform: rotate(-40deg);
+  }
+
+  .logo-group > * {
+    transition: transform .2s linear;
     transform-origin: center center;
   }
 
-  .logo-group:hover {
-    transform: rotate(25deg);
+  .logo:hover .top-triangle {
+    transform: translate(2%, 5%);
+  }
+
+  .logo:hover .right-triangle {
+    transform: translate(-5%, 1%);
+  }
+
+  .logo:hover .left-triangle {
+    transform: translate(2%, -2%);
   }
 
   /* logo img rules end */
@@ -48,12 +61,18 @@
     <router-link class="logo" to="/">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 310">
         <g class="logo-group">
-          <polygon class="triangle-one" points="50.3,60.5 117.8,71.5 140.9,136.5" />
-          <polygon class="triangle-two" points="117.8,71.5 163.5,22.5 140.9,136.5" />
+          <g class="top-triangle">
+            <polygon class="triangle-one" points="50.3,60.5 117.8,71.5 140.9,136.5" />
+            <polygon class="triangle-two" points="117.8,71.5 163.5,22.5 140.9,136.5" />
+          </g>
+          <g class="right-triangle">
+            <polygon class="triangle-one" points="287.5,126.5 242.8,177 173.6,162.5" />
+            <polygon class="triangle-two" points="242.8,177 262.3,240.3 173.6,162.5" />
+          </g>
+          <g class="left-triangle">
           <polygon class="triangle-one" points="112.2,287.5 88.9,224.9 135.9,173.4" />
           <polygon class="triangle-two" points="88.9,224.9 22.5,210.3 135.9,173.4" />
-          <polygon class="triangle-one" points="287.5,126.5 242.8,177 173.6,162.5" />
-          <polygon class="triangle-two" points="242.8,177 262.3,240.3 173.6,162.5" />
+          </g>
         </g>
       </svg>
     </router-link>
