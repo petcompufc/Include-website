834adae41f6f38c230b3d0dd6bdcf1997f1afaf8
Phillipe Queiroz <phgqm7@gmail.com>
2019-01-24 15:36:45 -0300

    Add map structure and map style to app.

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,15 +1,32 @@
 <style>
-  @import url("../../node_modules/leaflet/dist/leaflet.css");
+  #include-map {
+    width: 500px;
+    height: 300px;
+  }
 </style>
 
 <template>
   <section>
+    <div id="include-map"></div>
   </section>
 </template>
 
 <script>
 
-export default {
+import 'leaflet/dist/leaflet.css';
+import * as L from 'leaflet';
 
+const mapToken = 'https://api.mapbox.com/styles/v1/phqueiroz/cjraxr4jr2rok2ss1is4n357l/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGhxdWVpcm96IiwiYSI6ImNpeTUybWw5ZjAwNWQzMnAyczh5bXh3Z3oifQ.3LEj4_da9KlKXCESvTvXKA';
+let map;
+
+export default {
+  mounted() {
+    map = L.map('include-map').setView([-3.746020, -38.574175], 13);
+    
+    L.tileLayer(mapToken, {
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    }).addTo(map);
+  },
 };
 </script>
